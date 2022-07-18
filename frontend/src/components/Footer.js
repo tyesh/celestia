ef5a5a69a900d89784cca35b18902457ff44400c
carlos <carlos.velazquez91@gmail.com>
2022-07-18 16:15:09 -0400

    Added booststrap theme

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Col, Row } from 'react-bootstrap';
+
+const Footer = () => {
+  return (
+    <Container>
+      <Row>
+        <Col className='text-center py-3'>Copyright &copy; Celestia</Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Footer;
