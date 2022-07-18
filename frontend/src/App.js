ba1beb6542cb6609ea29979293a49a2fabfc079a
carlos <carlos.velazquez91@gmail.com>
2022-07-18 16:55:48 -0400

    Added redux to the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <main>
-        <h1>Welcome to Celestia</h1>
-      </main>
+      <Routes>
+        <Route path='/' element={<HomeScreen />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 };
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,8 @@
 import React from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux';
-import { logout } from '../actions/userActions';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
-  const dispatch = useDispatch();
-  const userLogin = useSelector((state) => state.userLogin);
-  const { userInfo } = userLogin;
-
-  const logoutHandler = () => {
-    dispatch(logout());
-  };
-
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
@@ -24,7 +14,7 @@ const Header = () => {
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='me-auto'>
               <LinkContainer to='/algotithms'>
-                <Nav.Link>Algotithms</Nav.Link>
+                <Nav.Link>Algorithms</Nav.Link>
               </LinkContainer>
               <LinkContainer to='/about'>
                 <Nav.Link>About</Nav.Link>
@@ -32,16 +22,6 @@ const Header = () => {
               <LinkContainer to='/contact'>
                 <Nav.Link>Contact</Nav.Link>
               </LinkContainer>
-            </Nav>
-            <Nav className='ml-auto'>
-              <SearchBox />
-              {userInfo && userInfo.isAdmin && (
-                <NavDropdown title='Admin' id='adminMenu'>
-                  <NavDropdown.Item onClick={logoutHandler}>
-                    Logout
-                  </NavDropdown.Item>
-                </NavDropdown>
-              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
